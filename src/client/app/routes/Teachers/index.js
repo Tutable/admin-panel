e82ea0863dab96378df273fcb280daf9e00280f9
gaurav <sharma02gaurav@gmail.com>
2018-07-10 11:46:20 +0530

    address for teachers

--- src/client/app/routes/Teachers/index.js
+++ src/client/app/routes/Teachers/index.js
@@ -104,7 +104,7 @@ class TeachersListing extends Component {
 						<th>Name</th>
 						<th>Linked account</th>
 						<th>Email</th>
-						{/* <th>address</th> */}
+						<th>address</th>
 						<th>Verification</th>
 						<th>Delete</th>
 						<th>Actions</th>
@@ -144,7 +144,7 @@ class TeachersListing extends Component {
 												teacher.email
 										}
 									</td>
-									{/* <td>{teacher.address.location}</td> */}
+									<td>{teacher.address  && teacher.address.location}</td>
 									<td>
 										{
 											teacher.isVerified ?
